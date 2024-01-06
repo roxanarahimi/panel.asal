b84fc9d17e3961098a3c5f91c4a621a737e3d3a5
Roxana <ms.roxanarahimi@gmail.com>
2024-01-06 18:29:07 +0330

    fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -136,7 +136,7 @@ class AuthController extends Controller
             $code = Redis::where('key', $request->mobile)->first();
             if ($user && $code) {
 
-                return response([$user, $code],500);
+                return response([$request['password'], $code],500);
                 if ($request['password'] == $code['value']) {
 
                     $token = $user->createToken('user')->accessToken;
